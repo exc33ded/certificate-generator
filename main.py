c5eb4451851e0bb902e48110edee062e5dcf7a6b
Mohammed Sarim <sarimisraiely12@gmail.com>
2023-09-28 21:55:35 +0530

    commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# Certificate Generator
+import cv2
+
+list_of_names = []
+
+def cleanup_date():
+    with open("names.txt") as file:
+        for line in file:
+            print(line)
+            
+cleanup_date()
+
